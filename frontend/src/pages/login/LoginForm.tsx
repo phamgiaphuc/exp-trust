5401d907527ead99287c93dac1c6280dc165a619
johnnybinh <thebinhnguyen2703@gmail.com>
2024-07-03 20:39:30 +0700

    Binh | minor change to login and register page

--- frontend/src/pages/login/LoginForm.tsx
+++ frontend/src/pages/login/LoginForm.tsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const LoginForm = () => {
   const inputStyle: string = 'border-2 outline-none h-16 text-lg p-2 rounded-lg';
   return (
-    <div className=' h-full flex-col flex justify-center  '>
+    <div className=' h-full w-8/12 flex-col flex justify-center  '>
       <div className='flex-col flex items-center w-full gap-8'>
-        <div className=' w-6/12'>
+        <div className=' w-8/12'>
           <h1 className='text-5xl pb-4 font-bold'>Welcome Back</h1>
           <h1 className='text-3xl'>Please enter login details below</h1>
           <div className='flex-row gap-2  text-3xl font-normal flex'>
@@ -16,7 +16,7 @@ const LoginForm = () => {
             </Link>
           </div>
         </div>
-        <form className='flex flex-col gap-4 w-6/12'>
+        <form className='flex flex-col gap-4 w-8/12'>
           <input className={inputStyle} type='text' placeholder='Email or Phone Number' />
           <input className={inputStyle} type='password' placeholder='Password' />
           <Link className='text-lg' to={'/'}>

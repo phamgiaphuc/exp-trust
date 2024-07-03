c9a6914625ed677e5dffbe090a9549e109ea4757
vUh0nGqUaNg <vuhongquangit@gmail.com>
2024-07-03 22:16:36 +0700

    Quang | Done Company Singup

---
+++ frontend/src/pages/register/company/CompanyForm.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const CompanyForm: React.FC = () => {
+  return (
+    <div className='h-full flex flex-col justify-center gap-4 w-full'>
+      <h1 className='text-5xl pb-4 font-bold'>Get Started</h1>
+      <div>
+        <h1 className='text-3xl '>Please enter the detail below</h1>
+        <h1 className='text-3xl '>
+          or sign in{' '}
+          {
+            <Link className='text-blue-600' to={'/login'}>
+              here
+            </Link>
+          }
+        </h1>
+      </div>
+      <form className='flex pt-4 gap-5 flex-col'>
+        <div className='flex pt-4 gap-5 flex-row'>
+          <input className={inputStyle} type='text' name='company-name' placeholder='Company Name' id='' />
+          <input className={inputStyle} type='text' name='representative' placeholder='Representative' id='' />
+        </div>
+        <div className='flex pt-4 gap-5 flex-row'>
+          <input className={inputStyle} type='email' name='business-email' placeholder='Business Email' id='' />
+          <input className={inputStyle} type='text' name='industry' placeholder='Industry' id='' />
+        </div>
+        <div className='flex pt-4 gap-5 flex-row'>
+          <input className={inputStyle} type='text' name='company-size' placeholder='Company Size' id='' />
+          <input className={inputStyle} type='text' name='role' placeholder='Role' id='' />
+        </div>
+        <div className='flex pt-4 gap-5 flex-row'>
+          <input className={inputStyle} type='password' name='password' placeholder='Password' id='' />
+          <input className={inputStyle} type='password' name='confirm-password' placeholder='Confirm Password' id='' />
+        </div>
+        <button className=' bg-[#702CFF] text-white text-2xl h-16 rounded-lg font-semibold' type='submit'>
+          Sign Up
+        </button>
+        <Link
+          to={'/signup/individual'}
+          className=' bg-gray-400 text-white text-2xl h-16 rounded-lg font-semibold flex justify-center items-center '
+          type='submit'
+        >
+          You’re an Individual ?
+        </Link>
+      </form>
+    </div>
+  );
+};
+
+export default CompanyForm;
+
+const inputStyle: string = ' border-2 outline-none h-16 text-lg p-2 rounded-lg w-1/2';
--- frontend/src/pages/register/company/CompanyPage.tsx
+++ frontend/src/pages/register/company/CompanyPage.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import login2 from '../../../assets/login2.png';
+import exptrust from '../../../assets/exptrust.png';
+import CompanyForm from './CompanyForm.tsx';
 
-const CompanyPage = () => {
-  return <div>CompanyPage</div>;
+const CompanyPage: React.FC = () => {
+  return (
+    <div className='flex flex-row w-screen bg-gradient-to-r p-4 from-[#EEFDFF] to-[#FDFDFD] h-screen'>
+      <div className='w-6/12 flex justify-start'>
+        <img src={exptrust} className='size-12 w-auto object-fit ml-[25px]' alt='' />
+        <CompanyForm />
+      </div>
+      <div className='w-6/12 flex justify-center'>
+        <img className='h-full  ' src={login2} alt='' />
+      </div>
+    </div>
+  );
 };
 
 export default CompanyPage;

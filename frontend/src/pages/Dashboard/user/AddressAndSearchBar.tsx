fb6ccf1259db3ccabd666186d35b64718e957e1e
vUh0nGqUaNg <vuhongquangit@gmail.com>
2024-07-06 12:25:25 +0700

    Quang | Fix search bar

--- frontend/src/pages/Dashboard/user/AddressAndSearchBar.tsx
+++ frontend/src/pages/Dashboard/user/AddressAndSearchBar.tsx
@@ -2,21 +2,21 @@ import { IoSearchOutline } from "react-icons/io5";
 
 const AddressAndSearchBar = () => {
   return (
-    <div className="flex flex-row items-center  w-full justify-between rounded-xl p-3">
+    <div className="flex flex-row items-center  w-full justify-between rounded-xl p-2">
       <div className="flex flex-col justify-between items-center ml-4 rounded-xl">
-        <h1 className='text-black font-inter text-[24px] font-[500] leading-normal'>
+        <h1 className='text-black font-inter text-2xl font-medium leading-normal'>
           Your Exp Trust Address is
         </h1>
-        <h1 className='text-[#2400FF] font-inter text-[11px] font-[400] leading-normal '>
+        <h1 className='text-[#2400FF] font-inter text-xs font-normal leading-normal '>
           0xE9eBF26D8bbdB42E66593F58380305B61813Feab
         </h1>
       </div>
-      <div className="w-96 h-11 flex items-center justify-start text-red-500 rounded-lg border-[1px] border-[#BABABA]">
+      <div className="w-96 h-11 flex items-center justify-start text-red-500 rounded-lg border-2 border-[#BABABA]">
         <IoSearchOutline className="ml-4 size-19"/>
         <input
           type="text"
           placeholder='Search'
-          className='w-full h-full outline-none text-black text-[13px] font-[400] pl-2 rounded-lg' />
+          className='w-full h-full outline-none text-black text-sm font-normal pl-2 rounded-lg' />
       </div>
     </div>
   );

d1e814d9ce64f9f8219ec4aa3cee79a019b0819c
Acus <acus1304pgphcm@gmail.com>
2024-07-06 21:47:17 +0700

    Phuc | Fix eslint style

--- frontend/src/pages/Dashboard/DashboardPage.tsx
+++ frontend/src/pages/Dashboard/DashboardPage.tsx
@@ -1,10 +1,10 @@
-import AddressandSearchBar from "./user/AddressAndSearchBar";
+import AddressandSearchBar from './user/AddressAndSearchBar';
 
 const DashboardPage = () => {
   return (
-  <div className="bg-blue-500 flex flex-col justify-end gap-6 min-h-screen w-5/6 border-r border-r-gray-400 p-4">
-    <AddressandSearchBar />
-  </div>
+    <div className='bg-blue-500 flex flex-col justify-end gap-6 min-h-screen w-5/6 border-r border-r-gray-400 p-4'>
+      <AddressandSearchBar />
+    </div>
   );
 };
 
--- frontend/src/pages/register/company/CompanyPage.tsx
+++ frontend/src/pages/register/company/CompanyPage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import login2 from '../../../assets/login2.png';
 import exptrust from '../../../assets/exptrust.png';
 import CompanyForm from './CompanyForm.tsx';
 import { Link } from 'react-router-dom';

0a9a7bb161d784f6f6d1e8f1ed3ba8a24e9b5b69
vUh0nGqUaNg <vuhongquangit@gmail.com>
2024-07-06 12:09:01 +0700

    Quang | Done search and address bar

--- frontend/src/pages/Dashboard/user/AddressAndSearchBar.tsx
+++ frontend/src/pages/Dashboard/user/AddressAndSearchBar.tsx
@@ -1,15 +1,26 @@
+import { IoSearchOutline } from "react-icons/io5";
+
 const AddressAndSearchBar = () => {
   return (
-    <div>
-      <div className="flex flex-col ">
-        <h1 className='text-black font-inter text-[24px] font-[500] leading-normal'>Your Exp Trust Address is</h1>
-        <h1 className='text-[#2400FF] font-inter text-[11px] font-[400] leading-normal '>0xE9eBF26D8bbdB42E66593F58380305B61813Feab</h1>
+    <div className="flex flex-row items-center  w-full justify-between rounded-xl p-3">
+      <div className="flex flex-col justify-between items-center ml-4 rounded-xl">
+        <h1 className='text-black font-inter text-[24px] font-[500] leading-normal'>
+          Your Exp Trust Address is
+        </h1>
+        <h1 className='text-[#2400FF] font-inter text-[11px] font-[400] leading-normal '>
+          0xE9eBF26D8bbdB42E66593F58380305B61813Feab
+        </h1>
       </div>
-      <div>
-
+      <div className="w-96 h-11 flex items-center justify-start text-red-500 rounded-lg border-[1px] border-[#BABABA]">
+        <IoSearchOutline className="ml-4 size-19"/>
+        <input
+          type="text"
+          placeholder='Search'
+          className='w-full h-full outline-none text-black text-[13px] font-[400] pl-2 rounded-lg' />
       </div>
     </div>
   );
 };
+
 
 export default AddressAndSearchBar;
--- frontend/src/pages/Dashboard/user/UserDashboard.tsx
+++ frontend/src/pages/Dashboard/user/UserDashboard.tsx
@@ -3,7 +3,7 @@ import AddressandSearchBar from "./AddressAndSearchBar";
 const UserDashboard = () => {
   return (
     <div className='bg-blue-200 h-screen p-4 flex flex-col gap-4'>
-      <div className='bg-blue-400 flex justify-between'>
+      <div className='bg-white flex justify-between rounded-[10px]'>
         <AddressandSearchBar />
       </div>
       <div className='bg-blue-400'>Dashboard</div>

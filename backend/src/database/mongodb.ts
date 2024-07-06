5968a466e0a08a6f07385021b921e36b0a584d4b
“hwHoai” <daohuuhoai2655@email.com>
2024-07-06 22:48:21 +0700

    Hoai|Add exp card

--- backend/src/database/mongodb.ts
+++ backend/src/database/mongodb.ts
@@ -1,13 +1,16 @@
 "use strict";
 
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const connectString = `mongodb+srv://admin:123@cluster0.aeysnuj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// dev
 
 class Database {
   constructor() {
     this.connect();
   }
+  //connect
   connect(type = "mongodb") {
     if (1 === 1) {
       mongoose.set("debug", true);

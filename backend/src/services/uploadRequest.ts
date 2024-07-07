c687c496b40b7c3b76a7432debd76f864aaa11da
Acus <acus1304pgphcm@gmail.com>
2024-07-07 08:56:18 +0700

    Phuc | fix be

--- backend/src/services/uploadRequest.ts
+++ backend/src/services/uploadRequest.ts
@@ -4,29 +4,31 @@ import storeDataToBC from "./storeDataToBC";
 import decodeData from "./decodeData";
 
 const uploadRequest = async (req: Request) => {
-  if (req) {
-    const { companyName, representative, compEmail, role, startDate, endDate } =
-      req.body;
-      console.log("newcard ::", role);
-
-    const newStartDate = new Date(startDate);
-    const newEndDate = endDate ? new Date(endDate) : new Date();
-    const exp: number = newEndDate.getFullYear() - newStartDate.getFullYear();
-
-    const newExpCard = await ExpCard.create({
-      verify: "pending",
-      role,
-      exp,
-      companyName,
-      startDate,
-    });
+  const {companyName, representative, compEmail, role, startDate, endDate, name, phoneNumber, email, proof_file, note} = req.body;
+  try {
+    let total_experience = 0;
+    if (!endDate) {
+      total_experience = new Date().getFullYear() - new Date(startDate).getFullYear();
+    }
+    total_experience = new Date(endDate).getFullYear() - new Date(startDate).getFullYear();
+    const userTicket = {
+      name: name,
+      email: email,
+      phone_number: phoneNumber,
+      position: role,
+      total_experience: total_experience,
+      begin_date: new Date(startDate),
+      end_date: new Date(endDate),
+      note: note,
+      proof_file: proof_file
+    }
+    const newExpCard = await ExpCard.create(userTicket);
     const [certificateData] = await ExpCard.find().sort({ createdAt: -1 });
     console.log(certificateData);
     await storeDataToBC(certificateData);
-    // await decodeData(publicKey);
-  }
-  return {
-    message: 'abc'
+    return newExpCard;
+  } catch (error: any) {
+    throw new Error(error);
   }
 };
 
